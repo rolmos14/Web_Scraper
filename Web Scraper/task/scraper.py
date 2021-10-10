82514b3cad4fa2d549e0e97ff5af62a10531a5a2
rolmos14 <olmos.rafa@gmail.com>
2021-10-10 19:53:40 +0200

    Stage 4/5. Save_news method looks for articles of type News. Save_to_file stores the News in separate files.

--- Web Scraper/task/scraper.py
+++ Web Scraper/task/scraper.py
@@ -26,19 +26,21 @@ class WebScraper:
                 article_text = soup.find("div", {"class": "c-article-body u-clearfix"}).text
                 self.save_to_file(article_title, article_text)
 
+    def save_to_file(self, article_title, article_text):
+        # Replace punctuation marks and whitespaces with underscores for the file name
+        translation_table = str.maketrans(string.punctuation + " ", "_" * (len(string.punctuation) + 1))
+        file_name = article_title.translate(translation_table) + ".txt"
+        with open(file_name, 'wb') as file:
+            # strip() to remove unnecessary whitespaces
+            file.write(article_text.strip().encode())
+            print("Content saved.")
+
     def status(self):
         if self.response.status_code != 200:
             print(f"The URL returned {self.response.status_code}")
             return False
         else:
             return True
-
-    def save_to_file(self, article_title, article_text):
-        translation_table = str.maketrans(string.punctuation + " ", "_" * (len(string.punctuation) + 1))
-        file_name = article_title.translate(translation_table) + ".txt"
-        with open(file_name, 'wb') as file:
-            file.write(bytes(article_text, encoding="utf-8"))
-            print("Content saved.")
 
 
 web_scraper = WebScraper("https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3")
